8758358f839dc3aaa54700d63f54a1e6984fb2a3
Härkönen Hiski <hiski.harkonen@aalto.fi>
2023-12-21 17:26:40 +0200

    1.2 Done

---
+++ part1/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = ({course}) => {
+    return (
+        <h1>{course}</h1>
+    )
+}
+
+const Part = ({part}) => {
+    return (
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    )
+}
+
+const Content = ({parts}) => {
+    return (
+        <>
+        <Part part={parts[0]} />
+        <Part part={parts[1]} />
+        <Part part={parts[2]} />
+        </>
+    )
+}
+
+const Total = ({exercises}) => {
+    return (
+        <p>Number of exercises {exercises.reduce((sum, x) => sum + x, 0)}</p>
+    )
+}
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    const exercises = [exercises1, exercises2, exercises3]
+
+    const parts = [
+        { name: part1, exercises: exercises1 },
+        { name: part2, exercises: exercises2 },
+        { name: part3, exercises: exercises3 },
+    ]
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total exercises={exercises} />
+        </div>
+    )
+}
+
+export default App
